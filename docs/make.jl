57ba92cd2fa86e69f56aceae54becf47853423aa
Vivak Patel <vp314@users.noreply.github.com>
2024-12-09 13:07:47 -0500

    Removed auto-doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,3 @@
-using Pkg 
-Pkg.instantiate()
-
 include("../LAModels/src/LAModels.jl")
 
 using Documenter, .LAModels
@@ -11,8 +8,4 @@ makedocs(
         "Linear Algebra Application Test Suite" => "index.md",
         "Linear Algebra Models" => "LAModels.md",
     ]
-)
-
-deploydocs(
-    repo = "github.com/numlinalg/LinearAlgebraApplications",
 )
