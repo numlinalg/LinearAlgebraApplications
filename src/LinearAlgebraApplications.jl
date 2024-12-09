42ca62f43e9bb1d1455980973a8c65917469e94c
Vivak Patel <vp314@users.noreply.github.com>
2024-12-09 11:38:27 -0500

    Added LAModels

--- src/LinearAlgebraApplications.jl
+++ src/LinearAlgebraApplications.jl
@@ -1,4 +1,5 @@
 # A collection of repositories for creating standardized application interfaces 
 # for RLinearAlgebra.jl
 
-include("../LAModels/src/LAModels.jl")
+
+include("../LAModels/src/LAModels.jl"); Pkg.add("LAModels")
